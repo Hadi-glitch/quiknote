71a415ebb584ebe39444257272ef6dfd847e7dc1
Hadi <abdulhadi74737@gmail.com>
2024-09-28 19:55:54 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: true, // Ignores ESLint errors during builds
+        ignoreDuringBuilds: true,
       },
 };
 
